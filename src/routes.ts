3dc878594d5795a916f9c85454825e5c92a2c27a
peterhendrick <peterjhendrick@gmail.com>
2019-02-09 18:14:09 -0700

    updating database rates on start and 60 second intervals and adding 10000 balance for new users

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,15 @@
 import { Pool } from 'pg';
 import { config } from './config';
 import { Request, Response } from 'express';
+import * as request from 'request-promise';
 const pool = new Pool(config);
 
+_getTickerAndSaveRates();
+setInterval(_getTickerAndSaveRates, 60000);
+
 const home = async (request: Request, response: Response) => {
-    response.json({ info: 'Node.js, Express, and Postgres API' });
+    const rates = await _getDatabaseRates();
+    response.json(rates);
 };
 
 const getUserById = async (request: Request, response: Response) => {
@@ -28,11 +33,18 @@ const getUsers = async (request: Request, response: Response) => {
 };
 
 const createUser = async (request: Request, response: Response) => {
-    const { name, email } = request.body;
+    const { name, email, password } = request.body;
+    if (!email || !password) return response.status(400).send(`${!email ? 'Email required ' : ''}${!password ? 'Password Required' : ''}`);
+    if (password.length !== 64) return response.status(400).send(`Invalid password: Must be a sha 256 hash.`);
 
     try {
-        const results = await pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email])
-        response.status(201).send(`User added with ID: ${results.insertId}`);
+        const existingUserCheck = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+        if (existingUserCheck.rowCount) return response.status(400).send(`User email is taken`);
+        const results = await pool.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3)', [name, email, password])
+        const idResponse = await pool.query('SELECT id FROM users WHERE email = $1', [email]);
+        const userId = idResponse.rows[0].id;
+        await pool.query('INSERT INTO balances (xmr, ltc, doge, salt, usd, userid) VALUES (0, 0, 0, 0, 10000, $1)', [userId]);
+        response.status(201).send(`User added with id: ${userId}`);
     } catch (error) {
         throw error;
     }
@@ -60,6 +72,49 @@ const deleteUser = async (request: Request, response: Response) => {
         throw error;
     }
 };
+
+async function _getTickerAndSaveRates() {
+    try {
+        const [xmrRes, ltcRes, dogeRes, saltRes, usdRes] = await Promise.all([
+            request('https://www.ShapeShift.io/rate/btc_xmr'),
+            request('https://www.ShapeShift.io/rate/btc_ltc'),
+            request('https://www.ShapeShift.io/rate/btc_doge'),
+            request('https://www.ShapeShift.io/rate/btc_salt'),
+            request('https://api.coinbase.com/v2/prices/spot?currency=USD')
+        ]);
+        const xmrRate = !xmrRes || JSON.parse(xmrRes).error ? null : JSON.parse(xmrRes).rate;
+        const ltcRate = !ltcRes || JSON.parse(ltcRes).error ? null : JSON.parse(ltcRes).rate;
+        const dogeRate = !dogeRes || JSON.parse(dogeRes).error ? null : JSON.parse(dogeRes).rate;
+        const saltRate = !saltRes || JSON.parse(saltRes).error ? null : JSON.parse(saltRes).rate;
+        const usdRate = !usdRes || JSON.parse(usdRes).error ? null : JSON.parse(usdRes).data.amount;
+
+        let saveResults = await _saveRates(xmrRate, ltcRate, dogeRate, saltRate, usdRate);
+
+        console.log();
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function _saveRates(xmrRate, ltcRate, dogeRate, saltRate, usdRate) {
+    const ratesResponse = await pool.query('SELECT * FROM rates ORDER BY id ASC');
+    const insertCheck = !(ratesResponse && ratesResponse.rowCount);
+    if (insertCheck) return await pool.query('INSERT INTO rates (xmr, ltc, doge, salt, usd, id) VALUES ($1, $2, $3, $4, $5, 1)', [xmrRate, ltcRate, dogeRate, saltRate, usdRate]);
+    const currentRates = ratesResponse.rows[0];
+    xmrRate = xmrRate ? xmrRate : currentRates.xmr;
+    ltcRate = ltcRate ? ltcRate : currentRates.ltc;
+    dogeRate = dogeRate ? dogeRate : currentRates.doge;
+    saltRate = saltRate ? saltRate : currentRates.salt;
+    usdRate = usdRate ? usdRate : currentRates.usd;
+
+    return await pool.query('UPDATE rates SET xmr = $1, ltc = $2, doge = $3, salt = $4, usd = $5 WHERE id = 1', [xmrRate, ltcRate, dogeRate, saltRate, usdRate]);
+    console.log();
+}
+
+async function _getDatabaseRates() {
+    const ratesResponse = await pool.query('SELECT * FROM rates ORDER BY id ASC');
+    return ratesResponse.rows[0];
+}
 
 export const routes = {
     createUser,
