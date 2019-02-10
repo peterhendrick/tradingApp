0b591cd62def285df50bddabfeb0a956569843ae
peterhendrick <peterjhendrick@gmail.com>
2019-02-10 15:23:24 -0700

    fixed tslint errors

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,13 @@
 import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
 import * as express from 'express';
-import { routes } from './routes';
 import * as path from 'path';
+import { routes } from './routes';
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'client/build')));
-
 
 app.use(bodyParser.json());
 app.use(
@@ -17,25 +18,26 @@ app.use(
 
 app.get('/', routes.home);
 app.get('/users', routes.getUsers);
+app.post('/users/authenticate', routes.authenticateUser);
 app.get('/users/:id', routes.getUserById);
 app.post('/users', routes.createUser);
 app.put('/users/:id', routes.updateUser);
 app.delete('/users/:id', routes.deleteUser);
 
 // An api endpoint that returns a short list of items
-app.get('/api/getList', (req,res) => {
-	var list = ["item1", "item2", "item9"];
-	res.json(list);
-	console.log('Sent list of items');
+app.get('/api/getList', (req: express.Request, res: express.Response) => {
+    const list = ['item1', 'item2', 'item9'];
+    res.json(list);
+    console.log('Sent list of items');
 });
 
 app.get('/api/home', (req, res) => {
     routes.home(req, res);
-})
+});
 
 // Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-	res.sendFile(path.join(__dirname+'/client/build/index.html'));
+app.get('*', (req: express.Request, res: express.Response) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 app.listen(port, () => {
