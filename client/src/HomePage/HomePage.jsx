87d56dc029af1cb382933527fe191b1d8bde4195
peterhendrick <peterjhendrick@gmail.com>
2019-02-10 23:04:43 -0700

    added balances and rates

--- client/src/HomePage/HomePage.jsx
+++ client/src/HomePage/HomePage.jsx
@@ -6,7 +6,7 @@ import { userActions } from '../_actions';
 
 class HomePage extends React.Component {
     componentDidMount() {
-        this.props.dispatch(userActions.getAll());
+        // this.props.dispatch(userActions.getAll());
     }
 
     handleDeleteUser(id) {
@@ -14,13 +14,27 @@ class HomePage extends React.Component {
     }
 
     render() {
-        const { user, users } = this.props;
+        const { user } = this.props;
+        const rates = JSON.parse(localStorage.getItem('rates'));
         return (
             <div className="col-md-6 col-md-offset-3">
                 <h3>Hi {user.email}!</h3>
                 <p>You're logged in with React!!</p>
-                <h3>All registered users:</h3>
-                {users.loading && <em>Loading users...</em>}
+                <h3>Your Balances: </h3>
+                <h4>btc: ₿{user.balances.btc}</h4>
+                <h4>xmr: ɱ{user.balances.xmr}</h4>
+                <h4>ltc: Ł{user.balances.ltc}</h4>
+                <h4>salt: Δ{user.balances.salt}</h4>
+                <h4>doge: Ð{user.balances.doge}</h4>
+                <h4>usd: ${user.balances.usd}</h4>
+                <h3>Current rates: </h3>
+                <h4>xmr: {rates.xmr} xmr/btc</h4>
+                <h4>ltc: {rates.ltc} ltc/btc</h4>
+                <h4>salt: {rates.salt} salt/btc</h4>
+                <h4>doge: {rates.doge} doge/btc</h4>
+                <h4>usd: {rates.usd} usd/btc</h4>
+                {/* <h3>All registered users:</h3> */}
+                {/* {users.loading && <em>Loading users...</em>}
                 {users.error && <span className="text-danger">ERROR: {users.error}</span>}
                 {users.items &&
                     <ul>
@@ -35,7 +49,7 @@ class HomePage extends React.Component {
                             </li>
                         )}
                     </ul>
-                }
+                } */}
                 <p>
                     <Link to="/login">Logout</Link>
                 </p>
@@ -45,11 +59,10 @@ class HomePage extends React.Component {
 }
 
 function mapStateToProps(state) {
-    const { users, authentication } = state;
+    const { authentication } = state;
     const { user } = authentication;
     return {
-        user,
-        users
+        user
     };
 }
 
