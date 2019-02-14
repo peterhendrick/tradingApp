4eea3a63038ee346de327854073f92068e8b4a06
peterhendrick <peterjhendrick@gmail.com>
2019-02-13 21:40:53 -0700

    updated sell route to match buy

--- server/routes.ts
+++ server/routes.ts
@@ -10,23 +10,23 @@ setInterval(_getTickerAndSaveRates, 60000);
 const getRates = async (req: Request, res: Response) => {
     const rates = await pool.query('SELECT * FROM rates ORDER BY id ASC')
         .then(_handleDBResults);
-    res.json({ok: true, text: rates});
+    res.json({ ok: true, text: rates });
 };
 
 const createUser = async (req: Request, res: Response) => {
     const { username, hashedPassword } = req.body;
     if (!username || !hashedPassword) return res.status(400)
         .send(`${!username ? 'Username required ' : ''}${!hashedPassword ? 'Password Required' : ''}`);
-    if (hashedPassword.length !== 64) return res.status(400).json({message: `Invalid password: Must be a sha 256 hash.`});
+    if (hashedPassword.length !== 64) return res.status(400).json({ message: `Invalid password: Must be a sha 256 hash.` });
     try {
         const existingUserCheck = await pool.query('SELECT * FROM users WHERE username = $1', [username.toLowerCase()]);
-        if (existingUserCheck.rowCount) return res.status(400).json({message: `User username is taken`});
+        if (existingUserCheck.rowCount) return res.status(400).json({ message: `User username is taken` });
         await pool.query('INSERT INTO users (username, password) VALUES ($1, $2)', [username.toLowerCase(), hashedPassword]);
         const idResponse = await pool.query('SELECT id FROM users WHERE username = $1', [username.toLowerCase()]);
         const userId = idResponse.rows[0].id;
         await pool.query('INSERT INTO balances (btc, xmr, ltc, doge, salt, usd, userid) VALUES (0, 0, 0, 0, 0, 10000, $1)',
-        [userId]);
-        res.status(201).json({ok: true, text: `User ${username.toLowerCase()} added successfully. You can now login.`});
+            [userId]);
+        res.status(201).json({ ok: true, text: `User ${username.toLowerCase()} added successfully. You can now login.` });
     } catch (error) {
         throw error;
     }
@@ -37,9 +37,9 @@ const authenticateUser = async (req: Request, res: Response) => {
     const user = await pool.query('SELECT * FROM users FULL OUTER JOIN balances ON users.id = balances.userid WHERE username = $1', [username.toLowerCase()])
         .then(_handleDBResults);
     if (user === '404' || user.password !== hashedPassword)
-        return res.status(404).json({message: 'Username or password is incorrect'});
+        return res.status(404).json({ message: 'Username or password is incorrect' });
     const responseJson = {
-        balances: {xmr: user.xmr, btc: user.btc, ltc: user.ltc, salt: user.salt, doge: user.doge, usd: user.usd},
+        balances: { xmr: user.xmr, btc: user.btc, ltc: user.ltc, salt: user.salt, doge: user.doge, usd: user.usd },
         id: user.id,
         token: 'fake-jwt-token',
         username: user.username
@@ -62,17 +62,26 @@ const buyBtc = async (req: Request, res: Response) => {
             pool.query('SELECT * FROM balances WHERE userid = $1', [id]),
             pool.query('SELECT * FROM rates WHERE id = 1')
         ])
-        .then((data) => [_handleDBResults(data[0]), _handleDBResults(data[1])]);
+            .then((data) => [_handleDBResults(data[0]), _handleDBResults(data[1])]);
         const pairBalance = Number(userBalances[pair]);
         const pairTradeAmount = Number(amount) * Number(rates[pair]);
         const btcBalance = Number(userBalances.btc);
         if (pairBalance >= pairTradeAmount) {
             const pairAmount = pairBalance - pairTradeAmount;
-            const btcAmount = btcBalance + amount;
-            await pool.query(`UPDATE balances SET ${pair} = ${pairAmount.toString()}, btc = ${btcAmount.toString()} WHERE userid = ${id}`);
-            res.status(200).json({ok: true, text: 'Trade Successful'});
+            const btcAmount = btcBalance + Number(amount);
+            const userBalance = await pool.query(`UPDATE balances SET ${pair} = ${pairAmount.toString()}, btc = ${btcAmount.toString()} WHERE userid = ${id} RETURNING *`)
+                .then(_handleDBResults);
+            let user = await pool.query(`SELECT * FROM users FULL OUTER JOIN balances ON users.id = balances.userid WHERE id = ${userBalance.userid}`)
+                .then(_handleDBResults);
+            user = {
+                balances: { xmr: user.xmr, btc: user.btc, ltc: user.ltc, salt: user.salt, doge: user.doge, usd: user.usd },
+                id: user.id,
+                token: 'fake-jwt-token',
+                username: user.username
+            };
+            res.status(200).json({ ok: true, text: { user, rates } });
         } else {
-            res.status(400).json({ok: true, text: ''});
+            res.status(400).json({ ok: false, text: `You do not have enough ${pair} to make trade.` });
         }
     } catch (error) {
         throw error;
@@ -86,17 +95,26 @@ const sellBtc = async (req: Request, res: Response) => {
             pool.query('SELECT * FROM balances WHERE userid = $1', [id]),
             pool.query('SELECT * FROM rates WHERE id = 1')
         ])
-        .then((data) => [_handleDBResults(data[0]), _handleDBResults(data[1])]);
+            .then((data) => [_handleDBResults(data[0]), _handleDBResults(data[1])]);
         const pairBalance = Number(userBalances[pair]);
         const pairTradeAmount = Number(amount) * Number(rates[pair]);
         const btcBalance = Number(userBalances.btc);
         if (btcBalance >= Number(amount)) {
             const pairAmount = pairBalance + pairTradeAmount;
-            const btcAmount = btcBalance - amount;
-            await pool.query(`UPDATE balances SET ${pair} = ${pairAmount.toString()}, btc = ${btcAmount.toString()} WHERE userid = ${id}`);
-            res.status(200).json({ok: true, text: 'Trade Successful'});
+            const btcAmount = btcBalance - Number(amount);
+            const userBalance = await pool.query(`UPDATE balances SET ${pair} = ${pairAmount.toString()}, btc = ${btcAmount.toString()} WHERE userid = ${id} RETURNING *`)
+                .then(_handleDBResults);
+            let user = await pool.query(`SELECT * FROM users FULL OUTER JOIN balances ON users.id = balances.userid WHERE id = ${userBalance.userid}`)
+                .then(_handleDBResults);
+            user = {
+                balances: { xmr: user.xmr, btc: user.btc, ltc: user.ltc, salt: user.salt, doge: user.doge, usd: user.usd },
+                id: user.id,
+                token: 'fake-jwt-token',
+                username: user.username
+            };
+            res.status(200).json({ ok: true, text: { user, rates } });
         } else {
-            res.status(400).json({ok: true, text: ''});
+            res.status(400).json({ ok: false, text: 'Not enough BTC to make trade.' });
         }
     } catch (error) {
         throw error;
@@ -129,7 +147,7 @@ async function _saveRates(xmrRate, ltcRate, dogeRate, saltRate, usdRate) {
     const insertCheck = !(ratesResponse && ratesResponse.rowCount);
     if (insertCheck) {
         return await pool.query('INSERT INTO rates (xmr, ltc, doge, salt, usd, id) VALUES ($1, $2, $3, $4, $5, 1)',
-        [xmrRate, ltcRate, dogeRate, saltRate, usdRate]);
+            [xmrRate, ltcRate, dogeRate, saltRate, usdRate]);
     }
     const currentRates = ratesResponse.rows[0];
     xmrRate = xmrRate ? xmrRate : currentRates.xmr;
@@ -139,7 +157,7 @@ async function _saveRates(xmrRate, ltcRate, dogeRate, saltRate, usdRate) {
     usdRate = usdRate ? usdRate : currentRates.usd;
 
     return await pool.query('UPDATE rates SET xmr = $1, ltc = $2, doge = $3, salt = $4, usd = $5 WHERE id = 1',
-    [xmrRate, ltcRate, dogeRate, saltRate, usdRate]);
+        [xmrRate, ltcRate, dogeRate, saltRate, usdRate]);
 }
 
 export const routes = {
