ee6c2c74b4a8a83faa905a378e9792d8332d87a9
peterhendrick <peterjhendrick@gmail.com>
2019-02-14 00:28:06 -0700

    serving built client

--- server/index.ts
+++ server/index.ts
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.use(bodyParser.json());
 app.use(
